7247a8d03f0df823ac99b7499a3bdcd0596a890b
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 10:45:46 +0600

    Generics

---
+++ Generics/CollectionInitializer.cs
@@ -0,0 +1,28 @@
+﻿namespace Generics
+{
+    public class CollectionInitializer<T>
+    {
+        private T[] collection;
+
+        public CollectionInitializer(int collectionLength)
+        {
+            collection = new T[collectionLength];
+        }
+        public void AddElementsToCollection(params T[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                collection[i] = elements[i];
+            }
+        }
+        public T[] RetriveAllElements()
+        {
+            return collection;
+        }
+
+        public T RetriveElementOnIndex(int index)
+        {
+            return collection[index];
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var initializer = new CollectionInitializer<int>(5);
+            initializer.AddElementsToCollection(1, 2, 3, 4, 5);
+            int[] arrayCollection = initializer.RetriveAllElements();
+
+            int number = initializer.RetriveElementOnIndex(3);
+
+            Console.WriteLine(number);
+        }
+
+    }
+}
