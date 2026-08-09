f02365b97b73591b56a6312b91a371007ef0198d
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 12:53:07 +0600

    Stack collection

---
+++ StackCollection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace StackCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(324);
+            stack.Push("Noman");
+            stack.Push(true);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
