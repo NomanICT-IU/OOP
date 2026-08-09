ba3f5394dbe9995ea896287a3d2642d312ba0074
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 17:36:21 +0600

    Func Delegate

---
+++ FuncDelegate/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> sumDelegate = Calculator.Sum;
+            Console.WriteLine(sumDelegate(10, 20));
+        }
+    }
+}
