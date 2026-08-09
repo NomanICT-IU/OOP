f80ac0001cf2554daaa0d4a3c8e4cc57046b99bd
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 12:46:06 +0600

    Queue

---
+++ QueueCollection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace QueueCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue();
+
+            queue.Enqueue(53);
+            queue.Enqueue(50);
+            queue.Enqueue("Noman");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
