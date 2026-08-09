6c22bf597d77d7848c335224f5deede393303a50
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 13:29:24 +0600

    Dictionary Collection

---
+++ DictionaryCollection/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DictionaryCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add("First", 174);
+            dictionary.Add("Second", 175);
+            dictionary.Add("Third", 176);
+            dictionary.Add("Fourth", 177);
+
+            foreach (var key in dictionary.Keys)
+            {
+                Console.WriteLine($"Key: {key}, value: {dictionary[key]}");
+            }
+        }
+    }
+}
