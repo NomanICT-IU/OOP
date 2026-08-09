2618bb7e13853f0e0cb134397ab7f9cfe767ce29
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 17:27:21 +0600

    Delegates

---
+++ Delegate/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Delegate
+{
+    public delegate void WriterDeletaget(string text);
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var writerDeletaget = new WriterDeletaget(Writer.Write);
+            writerDeletaget("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Delegate/Writer.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegate
+{
+    public static class Writer
+    {
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
