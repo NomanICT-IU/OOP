454d38f15ad3f9dd946375664bc03ea5a9b17662
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 17:44:46 +0600

    Action Delegate

---
+++ ActionDelegate/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ActionDelegate
+{
+    public static class Calculator
+    {
+        public static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ ActionDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ActionDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> actionDelegate = Calculator.Sum;
+            actionDelegate(10, 12);
+
+        }
+    }
+}
