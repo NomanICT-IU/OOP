378fb680cffe3d5bf5ce91dc73cac5b3c89deb9f
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 12:59:26 +0600

    HashTable Collection

---
+++ HashTableCollection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace HashTableCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashTable = new Hashtable();
+            hashTable.Add("First", 174);
+            hashTable.Add("Second", "Sixty");
+            hashTable.Add("Third", 124.24);
+            foreach (var key in hashTable.Keys)
+            {
+                Console.WriteLine($"Key: {key}, value: {hashTable[key]}");
+            }
+        }
+    }
+}
