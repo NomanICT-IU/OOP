22eb678897533d417c8daeae4dd82695aa345e29
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 11:03:48 +0600

    Generics Example

---
+++ GenericsExample/CollectionInitializer.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericsExample
+{
+    public class CollectionInitializer<T>
+    {
+        private T[] collection;
+        public CollectionInitializer(int collectionLength)
+        {
+            collection = new T[collectionLength];
+        }
+
+        public void AddElementToCollection(params T[] elemets)
+        {
+            for (int i = 0; i < elemets.Length; i++)
+            {
+                collection[i] = elemets[i];
+            }
+        }
+
+        public T[] RetriveCollection()
+        {
+            return collection;
+        }
+
+        public T RetriveElementOnIndex(int index)
+        {
+            return collection[index];
+        }
+    }
+}
---
+++ GenericsExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var initializer = new CollectionInitializer<int>(5);
+            initializer.AddElementToCollection(1, 2, 3, 4, 5);
+            int[] collection = initializer.RetriveCollection();
+            int retriveNumber = initializer.RetriveElementOnIndex(2);
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                Console.WriteLine(collection[i]);
+            }
+
+            Console.WriteLine($"Retive Eelement on index {retriveNumber}");
+        }
+    }
+}
