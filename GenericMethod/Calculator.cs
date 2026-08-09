c0d175b313b5ebfded445af63fd02b5f4302828c
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-08-09 11:23:35 +0600

    Generics Methods

---
+++ GenericMethod/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace GenericMethod
+{
+    public class Calculator<T> where T : INumber<T>
+    {
+
+
+        /// <summary>
+        /// Add method take 2 arguments
+        /// </summary>
+        /// <param name="param1"></param>
+        /// <param name="param2"></param>
+        /// <returns></returns>
+        public T Add(T param1, T param2)
+        {
+            return param1 + param2;
+        }
+    }
+}
---
+++ GenericMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator<double>();
+            double result = calculator.Add(2, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
